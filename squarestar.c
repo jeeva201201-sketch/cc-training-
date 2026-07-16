ec016bbce137bccc07d218ee8b4d5f874f11a7bb
Jeeva <jeeva201201@gmail.com>
2026-07-16 15:38:32 +0530

    Update squarestar.c

--- squarestar.c
+++ squarestar.c
@@ -1,10 +1,3 @@
-/******************************************************************************
-
-                            Online C Compiler.
-                Code, Compile, Run and Debug C program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
 
 #include <stdio.h>
 
@@ -21,9 +14,10 @@ int main() {
     }
 }
 
-o/p
+/*o/p
 *****
 *   *
 *   *
 *   *
 *****
+*/

45c11a0e65d34aa0abf528fcdd00ef2b64bee608
Jeeva <jeeva201201@gmail.com>
2026-07-16 15:37:30 +0530

    Clean up printtwonumbers.c and format output
    Removed unnecessary comments and adjusted output format.

--- printtwonumbers.c
+++ printtwonumbers.c
@@ -1,10 +1,4 @@
-/******************************************************************************
 
-                            Online C Compiler.
-                Code, Compile, Run and Debug C program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
 
 #include <stdio.h>
 
@@ -17,10 +11,10 @@ int main() {
         printf("%d%d ",i,j);
     }
 }
-o/p
+/*o/p
 5
 00 01 02 03 04 
 10 11 12 13 14 
 20 21 22 23 24 
 30 31 32 33 34 
-40 41 42 43 44 
+40 41 42 43 44 */
